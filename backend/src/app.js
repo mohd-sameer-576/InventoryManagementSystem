445436ba490a67e3dbc8a8704ebe59a5c3dc02fd
mohd-sameer-576 <xmohdsameerx576@gmail.com>
2026-02-10 21:41:19 +0530

    crateing server files

---
+++ backend/src/app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const httpStatus = require('http-status');
