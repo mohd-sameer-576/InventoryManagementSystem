d011ccc06671b5ada168f35a87181f1727903411
mohd-sameer-576 <xmohdsameerx576@gmail.com>
2026-02-19 19:23:18 +0530

    connected to the database

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const app = require('./src/app');
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const connectDB = require('./db/db');
+
+// Connect to MongoDB
+connectDB();
 
 // Middleware
 app.use(cors());
