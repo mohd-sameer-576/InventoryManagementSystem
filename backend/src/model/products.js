f336cde95382d7511b31d9e1c5c49a7dee10a0a6
mohd-sameer-576 <xmohdsameerx576@gmail.com>
2026-02-21 17:46:30 +0530

    changing products model

--- backend/src/model/products.js
+++ backend/src/model/products.js
@@ -4,7 +4,8 @@ const productsSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   category: { type: String, required: true },
-  quantity: { type: Number, required: true }
+  quantity: { type: Number, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 });
 
 const Products = mongoose.model('Products', productsSchema);
