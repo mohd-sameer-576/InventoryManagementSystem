f1e71de6c90c5fb46c61d22974199e3496befa17
mohd-sameer-576 <xmohdsameerx576@gmail.com>
2026-02-22 16:05:42 +0530

    createing cookie

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 
 async function signup(req, res) {
     try {
-        const {username , email, password, } = req.body;
+        const { username, email, password, } = req.body;
 
         // Check if user already exists
         const existingUser = await User.findOne({ $or: [{ username }, { email }] });
@@ -13,14 +13,26 @@ async function signup(req, res) {
         }
         const hashedPassword = await bcrypt.hash(password, 10);
         const token = jwt.sign(
-      { userId: user._id },
-      process.env.JWT_SECRET,
-      { expiresIn: "7d" }
-    );
+            { userId: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "7d" }
+        );
+        res.cookie("token", token, {
+            httpOnly: true,
+            sameSite: "none",
+            secure: true
+        });
         // create new user 
         const newUser = new User({ username, email, password: hashedPassword });
         await newUser.save();
-        res.status(201).json({ message: 'User created successfully', token });
+        res.status(201).json({
+            message: "User registered successfully",
+            user: {
+                _id: newUser._id,
+                username: newUser.username,
+                email: newUser.email
+            }
+        });
     } catch (error) {
         res.status(500).json({ message: 'Error creating user', error });
     }
@@ -36,9 +48,21 @@ async function login(req, res) {
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
             return res.status(400).json({ message: 'Invalid email or password' });
-        }   
+        }
         const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
-        res.json({ token });
+        res.cookie("token", token, {
+            httpOnly: true,
+            sameSite: "none",
+            secure: true
+        });
+        res.status(201).json({
+            message: "User Login successfully",
+            user: {
+                _id: user._id,
+                username: user.username,
+                email: user.email
+            }
+        });
     } catch (error) {
         res.status(500).json({ message: 'Error logging in', error });
     }
